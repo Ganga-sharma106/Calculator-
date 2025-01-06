539709a9e1f446c4163cccd8f93c2c408381ba74
Ganga-sharma106 <157630229+Ganga-sharma106@users.noreply.github.com>
2025-01-06 17:31:04 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,22 @@
+# Calculator //-
+def calculator():
+    print("Select operation: +, -, *, /")
+    operation = input("Enter operation: ")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if operation == "+":
+        print(f"Result: {num1 + num2}")
+    elif operation == "-":
+        print(f"Result: {num1 - num2}")
+    elif operation == "*":
+        print(f"Result: {num1 * num2}")
+    elif operation == "/":
+        if num2 != 0:
+            print(f"Result: {num1 / num2}")
+        else:
+            print("Error! Division by zero.")
+    else:
+        print("Invalid operation!")
+
+calculator()
